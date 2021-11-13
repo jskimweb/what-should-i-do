9d128bfb1822ed59101b1356a0bf934145d7552d
jskimweb <jskimweb@gmail.com>
2021-11-14 00:56:29 +0900

    Create project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Header></Header>
+  <Input></Input>
+  <List></List>
+  <Footer></Footer>
+</template>
+
+<script>
+import Header from '@/components/Header.vue';
+import Input from '@/components/Input.vue';
+import List from '@/components/List.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Input,
+    List,
+    Footer
+  }
+}
+</script>
+
+<style>
+
+</style>
